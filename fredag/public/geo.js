db6ed570508404b22cca61c2eb338cc1e011be05
Peter Berg <t4.peter.berg@umea.nti.se>
2017-12-18 13:49:30 +0100

    first commit

--- fredag/public/geo.js
+++
@@ -1,6 +0,0 @@
-function getLocation() {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            console.log("Latitude: " + position.coords.latitude +
-                    " Longitude: " + position.coords.longitude);
-        });
-}
